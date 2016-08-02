1477e44bd5cb8581b99cd8260195ab0dae7ab8cf
Helena Chen <helenach@sas.upenn.edu>
2016-08-02 15:08:43 -0400

    get index working

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('App is running on port', app.get('port'));
 });
