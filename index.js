6fbcc0dfd55ef0d248880085e387aa6058d80bb5
Helena Chen <helenach@sas.upenn.edu>
2016-08-02 16:16:18 -0400

    link index to html

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
   response.render('index');
