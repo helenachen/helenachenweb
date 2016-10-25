3e1b6b3c9d9cab1e1c8c6669957cf98dffeb17df
Helena Chen <helenach@sas.upenn.edu>
2016-10-24 22:23:35 -0400

    start over

--- public/javascripts/topnav.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-	var elem = document.getElementById("myTopnav");
-	if (elem.className == "topnav") {
-		elem.className += " responsive";
-	} else {
-		elem.className = "topnav";
-	}
-}
